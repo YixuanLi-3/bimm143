4d832493a6a5dc2f1945b834f3ccff0e7a824f35
Your Name <yil315@ucsd.edu>
2026-02-25 11:12:32 -0800

    Edit README locally on my computer

---
+++ Class4/Class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Plot for BIMM143 class04"
+#' author: "Yixuan li"
+#' date: "Jan 16, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x,sin(x),typ="l",col="red",lwd=3, xlab="Is it lunch time yet")
